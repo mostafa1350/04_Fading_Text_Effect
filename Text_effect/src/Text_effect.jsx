cd4c9ded43cf8de9e2e71d97e38beee1592cf848
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2024-12-27 00:17:43 +0330

    updating text effect ...
    I use about 6*360 degrees (rotate) and use x-1320px to lead in out of page ...

---
+++ Text_effect/src/Text_effect.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Text_effect = () => {
+  return (
+    <div className='min-h-screen flex flex-col justify-center items-center bg-blue-900'>
+        <ul className='ul_Text_Effect'>
+            <li className='li_Text'>I</li>
+            <li className='li_Text'>F</li>
+            <li className='li_Text'>N</li>
+            <li className='li_Text'>E</li>
+            <li className='li_Text'>V</li>
+            <li className='li_Text'>E</li>
+            <li className='li_Text'>R</li>            
+            <li className='li_Text'>.</li>            
+            <li className='li_Text'>C</li>            
+            <li className='li_Text'>O</li>            
+            <li className='li_Text'>M</li>            
+        </ul>
+        {/* <ul className='ul_Line'>
+            <hr className="Horizontal_Line"/>
+        </ul> */}
+        
+        
+    </div>
+  )
+}
+
+export default Text_effect
--- Text_effect/tailwind.config.js
+++ Text_effect/tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Times New Roman','ui-sans-serif', 'system-ui'],
+    },
     extend: {},
   },
   plugins: [],
