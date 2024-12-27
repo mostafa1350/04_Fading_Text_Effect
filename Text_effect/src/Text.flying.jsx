70a37bc11307c0c32a8226f5a9863d216be4ffbf
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2024-12-27 22:59:17 +0330

    make text flying file for animate it

---
+++ Text_effect/src/Text.flying.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Text_flying = () => {
+    return (
+        <div className='min-h-screen flex flex-col justify-center items-center bg-slate-300'>
+            <ul>
+                <li>
+                    ifnever.com
+                </li>
+            </ul>
+            <ul>
+                <li className='text-[20px] p-[10px] mt-[150px] text-slate-700 border-[1px] rounded-[4px] border-blue-600 shadow-2xl shadow-slate-400 hover:text-blue-800 hover:text-[25px] hover:duration-500 hover:border-[1px] hover:border-red-500 hover:p-[10px]'>
+                    <Link to='/'>
+                        Text Effect Page
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Text_flying
