9ed12f39f01a8bf8609c94389664088fdd38eb52
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2024-12-28 23:35:28 +0330

    create effect in text flying file

--- Text_effect/src/Text.flying.jsx
+++ Text_effect/src/Text.flying.jsx
@@ -4,11 +4,10 @@ import { Link } from 'react-router-dom'
 const Text_flying = () => {
     return (
         <div className='MainFlying'>
-            <ul>
-                <li>
-                    ifnever.com
-                </li>
-            </ul>
+            <div className='box'>
+                <span>ifnever.com</span>
+                
+            </div>
             <ul>
                 <li className='text-[20px] p-[10px] mt-[150px] text-slate-700 border-[1px] rounded-[4px] border-blue-600 shadow-2xl shadow-slate-400 hover:text-blue-800 hover:text-[25px] hover:duration-500 hover:border-[1px] hover:border-red-500 hover:p-[10px]'>
                     <Link to='/'>
